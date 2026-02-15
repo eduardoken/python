f256b804576f89232c40438c08c8a6aebc69c361
Eduardo Kenzo <edukenzo428@gmail.com>
2026-02-15 20:07:55 -0300

    fim exercício

--- tutorial/execício.py
+++ tutorial/execício.py
@@ -1,2 +1,14 @@
-nome = input('Digite o seu nome: ')
-print('Olá,', nome, 'Seja muito bem vindo aos meus projetos do curso em Python!')
+nome = str(input('Digite o seu nome: '))
+print('Olá,', nome, 'Seja muito bem vindo aos meus projetos de estudo do curso em Python!')
+
+print('Vamos começar a fazer os testes de tutoriais?')
+print(' Digite o dia, em seguida o mês e o ano do seu nascimento: ')
+
+dia = int(input('Dia: '))
+mes = input('Mês: ')
+ano = int(input('Ano: '))
+
+print('Você nasceu em', dia, mes, ano)
+
+soma = dia + ano
+print("A soma da sua data de nascimento com o ano é:", soma)
