8e60f5748c6501dcaecd1555219739527886995c
Eduardo Kenzo <edukenzo428@gmail.com>
2026-02-16 13:59:51 -0300

    ex011

---
+++ python_exercicios/ex011.py
@@ -0,0 +1,5 @@
+l = float(input('Qual a largura da parede em metros: '))
+a = float(input('Qual a altura da parede em metros: '))
+area = l * a
+tinta = area / 2
+print('Será necessário {} litros de tinta'.format(tinta))
