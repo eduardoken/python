60e63a5065e8640440664275b0f975a7cef7f85c
Eduardo Kenzo <edukenzo428@gmail.com>
2026-02-16 13:19:07 -0300

    ex006

---
+++ python_exercicios/ex006.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digite um número inteiro: '))
+dobro = n1 * 2
+triplo = n1 * 3
+raiz = n1 ** (1/2)
+print('o número {} tem seu dobro {}, seu triplo {} e sua raiz quadrada {}'.format(n1, dobro, triplo, raiz))
