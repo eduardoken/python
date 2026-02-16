4afaa72b323a89a25cee96255b2f32622b661c3f
Eduardo Kenzo <edukenzo428@gmail.com>
2026-02-16 12:45:12 -0300

    ex004

---
+++ python_exercicios/ex004.py
@@ -0,0 +1,6 @@
+n1 = input('Digite algo: ')
+n2 = int(input('Digite um numero: '))
+
+print('o tipo do primeiro é',type(n1),'o tipo do segundo é' ,type(n2))
+print('o primeiro é númerico?',n1.isnumeric())
+print('o primeiro é todo maiusculo?',n1.isupper())
