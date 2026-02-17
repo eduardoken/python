f5b5dfa11b51f8d7661c6b8ce7f32e217e0cc8d1
Eduardo Kenzo <edukenzo428@gmail.com>
2026-02-17 12:13:36 -0300

    ex025

--- python_exercicios/ex024.py
+++ python_exercicios/ex024.py
@@ -2,3 +2,4 @@ cidade = input('Informe o nome da sua cidade: ')
 cidade = cidade.capitalize()
 print('Sua cidade começa com o nome Santo?')
 print('Santo' in cidade)
+#Usei captalize e deixei Santo maiusculo, evitando indentificar santo na posição 0
---
+++ python_exercicios/ex025.py
@@ -0,0 +1,4 @@
+nome = input('Informe o seu nome: ')
+nome = nome.upper()
+print('Seu nome possui Silva?')
+print('SILVA' in nome)
