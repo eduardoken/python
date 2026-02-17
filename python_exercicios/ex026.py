5f2ddc0860a836bfe88ddd5821023a7b4b9fe834
Eduardo Kenzo <edukenzo428@gmail.com>
2026-02-17 12:33:47 -0300

    ex026

---
+++ python_exercicios/ex026.py
@@ -0,0 +1,6 @@
+frase = input('Digite uma frase: ')
+l = 1 + frase.find('A')
+r = 1 + frase.rfind('A')
+print('A frasse possui {} letras A'.format(frase.count('A')))
+print('A letra A se inicia na {}° posição'.format(l))
+print('A letra A se termina na {}° posição'.format(r))
