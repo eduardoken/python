12d06cb034d8589b35ef8a8ba817c1221bb5dc91
Eduardo Kenzo <edukenzo428@gmail.com>
2026-02-17 13:02:30 -0300

    leve alterações

--- python_exercicios/ex026.py
+++ python_exercicios/ex026.py
@@ -4,3 +4,4 @@ r = 1 + frase.rfind('A')
 print('A frasse possui {} letras A'.format(frase.count('A')))
 print('A letra A se inicia na {}° posição'.format(l))
 print('A letra A se termina na {}° posição'.format(r))
+#a maiuscula
